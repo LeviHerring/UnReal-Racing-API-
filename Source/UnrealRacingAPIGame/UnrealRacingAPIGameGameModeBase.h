36c6926868d60ec7aee52b37c6050660755c5c92
DESKTOP-ONOO3MD\theob <leviherring16@gmail.com>
2023-10-17 00:04:27 +0100

    FIrst Commit

---
+++ Source/UnrealRacingAPIGame/UnrealRacingAPIGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealRacingAPIGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALRACINGAPIGAME_API AUnrealRacingAPIGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
